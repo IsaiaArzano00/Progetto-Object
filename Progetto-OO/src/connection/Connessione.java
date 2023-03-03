0e6ac8d7f88e20fb3dbec44509f4f6c882418272
dNicob <nicola.dangelo3@studenti.unina.it>
2023-03-03 14:42:16 +0100

    recuperato branchNicola

--- Progetto-OO/src/connection/Connessione.java
+++ Progetto-OO/src/connection/Connessione.java
@@ -3,60 +3,78 @@ package connection;
 import java.sql.*;
 
 public class Connessione {
-	
-	private static Connection connection;
-	private static Connessione istanza ;
-	private Statement statement ;
-	
-	
-	private  Connessione ()
-	{
-		statement = null;
-		
+
+    private static Connection connection;
+    private static Connessione istanza;
+    private Statement statement;
+    private PreparedStatement prpdstmt;
+    private CallableStatement clbStmt;
+    
+    
+    
+    private Connessione() {
+        //inizializzo lo statement
+        statement = null;
+
+        try {
+            //carico i driver per la connessione al db
+            Class.forName("org.postgresql.Driver");
+
+        } catch (ClassNotFoundException e) {
+
+            System.err.println(e);
+        }
+
+        try {
+            // provo ad effettuare la connessione
+            String url = "jdbc:postgresql://localhost:5433/postgres";
+            connection = DriverManager.getConnection(url, "postgres", "1234");
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+            System.out.println("Errore in fase di connessione");
+
+        }
+
+        try {
+            //se la connessione è andata a buon fine istanzio lo statement
+            statement = connection.createStatement();
+            System.out.println("Connessione OK!");
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+        
+       
+    }
+    //metodo per ritornare la connessione ottenuta
+    public static Connessione getConnessione() {
+
+        if (istanza == null)
+            istanza = new Connessione();
+
+        return istanza;
+    }
+    //ritorno lo statement istanziato
+    public Statement getStatement() {
+
+        return statement;
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+    
+    public CallableStatement prepareCall(String string) {
 		try {
-
-			Class.forName("org.postgresql.Driver");
-
-		} catch (ClassNotFoundException e) {
-
-			System.err.println(e);
-		}
-		
-		try {
-
-			String url = "jdbc:postgresql://localhost:5432/ProvaPG?currentSchema=progettobdd";
-			connection = DriverManager.getConnection(url, "postgres", "123");
-
+			clbStmt = connection.prepareCall(string);
 		} catch (SQLException e) {
-
-			e.printStackTrace();
-			System.out.println("Tentativo di connessione fallito");
-
-		}
-
-		
-		try {
-
-			statement = connection.createStatement();
-
-		} catch (SQLException e) {
-
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-	}
-
-	public static Connessione getConnessione() {
-
-		if (istanza == null)
-			istanza = new Connessione();
-
-		return istanza;
-	}
-
-	public Statement getStatement() {
-
-		return statement;
+		return clbStmt;
 	}
 
 }
